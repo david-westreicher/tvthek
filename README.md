12a227170eef8d93c4fe0c3bff8a1cf8c65def08
david westreicher <david.westreicher@gmail.com>
2013-11-24 22:42:51 +0100

    initial

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-tvthek
+tvthek-parser
 ======
+Parses a tvthek.orf.at link and outputs the containing movie urls.
 
-tvthek
+Usage
+-----
+
+```
+java -jar tvthek.jar http://tvthek.orf.at/program/ZIB-2/1211/ZIB-2/7165715
+```
+
+Output:
+
+Signation
+
+http://apasfpd.apa.at/cms-worldwide/online/267e7ab1d885cc7afcbf1e7148b41574/1385326862/2013-11-22_2210_tl_02_ZIB-2_Signation__7165715__o__0000934536__s7165792___on_ORF2HiRes_22100006P_22104409P_Q6A.mp4
+
+Koalitions-Verhandlungen gehen voran
+
+http://apasfpd.apa.at/cms-worldwide/online/298d6b20812193211e3c07ed4a79d096/1385326862/2013-11-22_2210_tl_02_ZIB-2_Koalitionsverhandlun__7165715__o__0000934537__s7165793___an_ORF2HiRes_22104409P_22135007P_Q6A.mp4
+
+Gespräch mit Karmasin und Filzmaier
+
+http://apasfpd.apa.at/cms-worldwide/online/7d550218728b3b449cf553a6ae8916af/1385326862/2013-11-22_2210_tl_02_ZIB-2_Gespraech-mit-Karmas__7165715__o__0000934538__s7165794___er_ORF2HiRes_22135007P_22211508P_Q6A.mp4
+
+
+...
---
+++ src/tvthek/Parser.java
@@ -0,0 +1,128 @@
+package tvthek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+import com.google.gson.Gson;
+
+public class Parser {
+
+	private String site;
+
+	public Parser(String site2) {
+		this.site = site2;
+	}
+
+	public static void main(String[] args) {
+		new Parser(args[0]).parse();
+	}
+
+	private void parse() {
+		try {
+			URL url = new URL(site);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // Cast
+																				// shouldn't
+																				// fail
+			HttpURLConnection.setFollowRedirects(true);
+			// allow both GZip and Deflate (ZLib) encodings
+			// conn.setRequestProperty("Accept-Encoding", "gzip, deflate");
+			conn.setRequestProperty(
+					"User-agent",
+					"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/30.0.1599.114 Chrome/30.0.1599.114 Safari/537.36");
+			String encoding = conn.getContentEncoding();
+			InputStream inStr = null;
+
+			// create the appropriate stream wrapper based on
+			// the encoding type
+			if (encoding != null && encoding.equalsIgnoreCase("gzip")) {
+				inStr = new GZIPInputStream(conn.getInputStream());
+			} else if (encoding != null && encoding.equalsIgnoreCase("deflate")) {
+				inStr = new InflaterInputStream(conn.getInputStream(),
+						new Inflater(true));
+			} else {
+				inStr = conn.getInputStream();
+			}
+			BufferedReader in = new BufferedReader(new InputStreamReader(inStr));
+
+			String line;
+			int count = 0;
+			while ((line = in.readLine()) != null)
+				if (line.contains("<div class=\"jsb_ jsb_VideoPlaylist\" data-jsb=")
+						|| count > 0 && count < 1) {
+					count++;
+					String json = line.substring(line.indexOf("{"),
+							Math.min(line.lastIndexOf("}") + 1, line.length()));
+					json = json.replaceAll("&quot;", "\"");
+					json = json.replaceAll("&amp;", "&");
+					json = json.replaceAll("\\\\/", "/");
+					// System.out.println(json);
+					System.out.println(new Gson().fromJson(json,
+							VideoInformation.class));
+				}
+			in.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public class VideoInformation {
+		@Override
+		public String toString() {
+			return playlist.toString();
+		}
+
+		public Playlist playlist;
+	}
+
+	public class Playlist {
+		@Override
+		public String toString() {
+			String ret = "";
+			for (Video v : videos)
+				ret += v.toString();
+			return ret;
+		}
+
+		public Video[] videos;
+	}
+
+	public class Video {
+		@Override
+		public String toString() {
+			String ret = "";
+			for (Source s : sources)
+				if (s.quality_string.equals("hoch")
+						&& s.protocol.equals("http")
+						&& (s.delivery.equals("progressive") || s.delivery
+								.equals("hls")))
+					ret += s.toString() + "\n";
+			return title + "\n" + ret + "\n";
+		}
+
+		public String title;
+		public Source[] sources;
+	}
+
+	public class Source {
+		public String delivery;
+		public String quality_string;
+		public String src;
+		public String protocol;
+
+		@Override
+		public String toString() {
+			return src;
+		}
+	}
+}
